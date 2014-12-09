dcf1dada54884e465a3a33581012c8f360dde141
Murray Colpman <muzer@tim32.org>
2014-12-09 01:44:56 +0000

    Add data logger wrapper

---
+++ data_logger.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import trackernet_access
+import database_access
+import linedefs
+import time
+
+def main():
+    connection = database_access.connectToDb()
+    while True:
+        for line, station in linedefs.victoria:
+            print("Getting station {line}/{station}.".format(line=line,
+                station=station))
+            stationObj = trackernet_access.getStation(line, station)
+            database_access.addStationTree(connection, stationObj)
+            print("Done")
+            # sleep such that the time between gets will be 30 seconds,
+            # factoring in get time of about half a second
+            time.sleep((30-0.5*len(linedefs.victoria))/len(linedefs.victoria))
+
+main()
